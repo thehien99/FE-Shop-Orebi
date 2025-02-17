1add77dbfba45b8fdea8f7b6fcc58ee0d5d1f585
thehien99 <116476501+thehien99@users.noreply.github.com>
2025-02-17 11:05:31 +0700

    '1'

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import PuffLoader from 'react-spinners/PuffLoader'
+import Cookies from 'js-cookie'
+import { logoutSuccess } from '../redux/reducers/authReducer'
 
 const Loading = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const refreshToken = Cookies.get('rftk')
+    if (!refreshToken) {
+      dispatch(logoutSuccess())
+    }
+  }, [dispatch])
   return (
     <div className='absolute xs:left-[35%] top-[300px] left-[46%]'>
       <PuffLoader

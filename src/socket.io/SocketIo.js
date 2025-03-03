23d3475dae30ba28ab6424dec1807563fcf31611
thehien99 <116476501+thehien99@users.noreply.github.com>
2025-03-03 16:33:38 +0700

    'responsive'

--- src/socket.io/SocketIo.js
+++ src/socket.io/SocketIo.js
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // Kết nối đến backend (http://localhost:8080)
-const socket = io('http://localhost:8080', {
+const socket = io(import.meta.env.VITE_API_KEY, {
     autoConnect: true,
     withCredentials: true,  // Đảm bảo cookie hoặc session được gửi đi nếu cần
     transports: ['websocket'],  // Dùng WebSocket để kết nối (không dùng polling)
